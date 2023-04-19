d0d991fdf63a91211be9180e85c1db4d8549a743
OlgaTora <bestlaly@mail.ru>
2023-04-19 18:31:04 +0300

    add new task: gunicorn+nginx

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@ sudo ln -sf home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/gunicorn restart
+
+sudo gunicorn --bind 127.0.0.1:8080 hello:wsgi_application
+
+
